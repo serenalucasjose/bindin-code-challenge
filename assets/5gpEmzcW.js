798e4cfa5148234e4eca8151cde6d6571600df4f
Lucas Serena <serenalucasjose@gmail.com>
2025-03-05 18:47:56 -0300

    Updates

---
+++ assets/5gpEmzcW.js
@@ -0,0 +1,1 @@
+import{p as t}from"./Ckwlw83O.js";const n=t("formStore",{state:()=>({forms:[]}),actions:{createForm(o){const s={id:this.forms.length+1,questions:o,responses:[]};this.forms.push(s)},respondForm(o,s){const r=this.forms.find(e=>e.id===o);r&&r.responses.push(s)},getFormResponses(o){const s=this.forms.find(r=>r.id===o);return s?s.responses:[]}}});export{n as u};
